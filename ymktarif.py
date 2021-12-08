e52a7546ab485d07dac60f5a7b0c813279528aed
Mustafa Celenk <63021808+mustafacelenkx@users.noreply.github.com>
2021-12-08 22:38:53 +0300

    Add files via upload

---
+++ ymktarif.py
@@ -0,0 +1,17 @@
+print(" ███ -- Mustafa Celenk -- ███")
+class Yemek:
+  x = input("Yemek Adi Gir = ")
+
+p1 = Yemek()
+
+class Malzeme:
+    y = input("Malzemeleri Gir = ")
+p2 = Malzeme()
+
+
+ymktarif = []
+
+ymktarif.append(p1.x)
+ymktarif.append(p2.y)
+
+print(ymktarif)
